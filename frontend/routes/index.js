dcdcbdb17c67c5178e6034e33c466f004593f684
ogbaududavid <ogbaudu.dc@gmail.com>
2023-05-02 09:12:12 +0100

    changed routes for week2 team assignment

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -1,6 +1,6 @@
 const controllers = require("../controllers/index");
 const route = require("express").Router();
-route.get("/", controllers.route1);
-route.get("/route2", controllers.route2);
+route.get("/route3", controllers.route1);
+route.get("/route4", controllers.route2);
 
 module.exports = route;
