097695c0741fbd5f349500f968d152134b5adea8
davidogbaudu <ogbaudu.dc@gmail.com>
2023-10-07 14:08:59 +0100

    added swagger routes

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const mongodb = require("./db/connect");
 var app = express();
 const port = process.env.PORT || 8080
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json')
+
 app
   .use(bodyParser.json())
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     next();
---
+++ swagger.js
@@ -0,0 +1,20 @@
+const swaggerAutogen = require('swagger-autogen')()
+
+const doc = {
+    info: {
+        title: "contacts API",
+        description: "An api that allows your retrive, post, and delete contacts from a server"
+    },
+    host: 'https://cse341-services.onrender.com',
+    schemes: ['https']
+}
+
+const outputFile = './swagger.json'
+const endpointsFiles = ['./routes/index.js']
+
+// swaggerAutogen(outputFile, endpointsFiles, doc)
+
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+    await import('./app.js'); // Your project's root file
+  });
